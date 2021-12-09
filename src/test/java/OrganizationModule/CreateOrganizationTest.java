ed80d76f47df8d708e46a80610fba53e13dc4e4e
mrudu <mrudu@LAPTOP-PF1SH7SD>
2021-12-09 22:32:28 +0530

    User-2 created New Organization -commit-1

---
+++ src/test/java/OrganizationModule/CreateOrganizationTest.java
@@ -0,0 +1,14 @@
+package OrganizationModule;
+import org.testng.annotations.Test;
+
+
+
+public class CreateOrganizationTest {
+	@Test
+	public void createOrganizationTest() {
+		System.out.println("Organiztion Method created - User-2, commit-1");
+		
+	}
+	
+
+}
