2414141c2bf2a37c7548b69c0a482f9265ea9b58
mrudu <mrudu@LAPTOP-PF1SH7SD>
2021-12-09 22:46:48 +0530

    organization modified by Padhu, Commit-2

--- src/test/java/OrganizationModule/CreateOrganizationTest.java
+++ src/test/java/OrganizationModule/CreateOrganizationTest.java
@@ -7,6 +7,7 @@ public class CreateOrganizationTest {
 	@Test
 	public void createOrganizationTest() {
 		System.out.println("Organiztion Method created - User-2, commit-1");
+		System.out.println(" Organization is modified by Padhu, commit-2");
 		
 	}
 	
