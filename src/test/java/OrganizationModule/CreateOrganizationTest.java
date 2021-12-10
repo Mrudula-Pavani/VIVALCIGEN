2eff2735f91585ff98effe1c170a96efc19dcbbc
mrudu <mrudu@LAPTOP-PF1SH7SD>
2021-12-10 05:47:36 +0530

    Changes made to Organization by User-2

--- src/test/java/OrganizationModule/CreateOrganizationTest.java
+++ src/test/java/OrganizationModule/CreateOrganizationTest.java
@@ -10,6 +10,7 @@ public class CreateOrganizationTest {
 		System.out.println(" Organization is modified by Padhu, commit-2");
 		System.out.println("Modification done in Github");
 		System.out.println("Create a pull request , commit-5");
+		System.out.println("Changes done by User-2, commit-7");
 		
 	}
 	
